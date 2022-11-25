c8db7a5a7e8e54ba2ac3c041570fe0c8ef188453
daryn <akishevdarin@gmail.com>
2022-11-25 13:17:35 +0600

    settings

---
+++ ios/Flutter/flutter_export_environment 2.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/darynakishev/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/darynakishev/Documents/dev/flutter/coffee"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/darynakishev/Documents/dev/flutter/coffee/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/darynakishev/Documents/dev/flutter/coffee/.dart_tool/package_config.json"
